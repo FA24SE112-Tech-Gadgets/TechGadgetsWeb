327cf425ea463f3c27c371a509666bbe19b3618f
thong312 <lyminhthong312@gmail.com>
2024-12-01 19:39:53 +0700

    fix_v02

--- src/pages/Seller/Gadgets/GadgetManagementPage.js
+++ src/pages/Seller/Gadgets/GadgetManagementPage.js
@@ -36,7 +36,7 @@ const GadgetManagementPage = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <ToastContainer />
+      {/* <ToastContainer /> */}
       <div className="flex flex-col space-y-4 mb-6">
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold">Quản lý sản phẩm</h1>
