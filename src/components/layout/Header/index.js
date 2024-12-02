f7d908d7b40a6d6ac5cbfdaf77af444e8b307174
kietpt2003 <tuankiet29012003@gmail.com>
2024-12-02 22:43:57 +0700

    Update

--- src/components/layout/Header/index.js
+++ src/components/layout/Header/index.js
@@ -155,7 +155,7 @@ const Header = () => {
           <div>
             <button onClick={() => navigate("/")} className="font-bold text-2xl sm:text-3xl flex gap-2">
               <img src={Logo} alt="Logo" className="w-10" />
-              Tech Gadget
+              Tech Gadgett
             </button>
           </div>
 
